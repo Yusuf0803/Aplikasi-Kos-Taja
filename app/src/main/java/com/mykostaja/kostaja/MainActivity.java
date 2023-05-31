909d8db7298cd1bfe4556961643b1a258a48dbd5
Yusuf0803 <baeyusuf094@gmail.com>
2023-05-31 20:27:30 +0700

    Update terbaru

--- app/src/main/java/com/mykostaja/kostaja/MainActivity.java
+++ app/src/main/java/com/mykostaja/kostaja/MainActivity.java
@@ -3,8 +3,11 @@ package com.mykostaja.kostaja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TableLayout tableLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
