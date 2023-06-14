c3f594eb9c4eb6f3e2a23fdd399dc5f1d12bbca0
Az-Rof <rofiazhar002@gmail.com>
2023-06-15 06:18:44 +0700

    Ya seperti itu lah

--- app/src/main/java/com/mykostaja/kostaja/Pencari/profil_user.java
+++ app/src/main/java/com/mykostaja/kostaja/Pencari/profil_user.java
@@ -13,19 +13,33 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mykostaja.kostaja.Login;
 import com.mykostaja.kostaja.Pemilik.profil_admin;
 import com.mykostaja.kostaja.R;
+import com.mykostaja.kostaja.User;
 import com.mykostaja.kostaja.edit_profil;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.text.BreakIterator;
 
 public class profil_user extends AppCompatActivity {
 
     private CardView profil_user;
     private ImageView gambar_profil_user;
 
-    private TextView nama_profil_user, edit_profil_user;
-    private TextView pengaturan_profil_user,riwayat_profil_user,bantuan_profil_user,keluar_profil_user;
+    String getPhone,getUserame;
+    private TextView nama_profil_user,info_profil_user,nohp_user;
+    private TextView riwayat_profil_user,bantuan_profil_user,keluar_profil_user;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -36,14 +50,41 @@ public class profil_user extends AppCompatActivity {
         gambar_profil_user = findViewById(R.id.gambar_profil_user);
 
         nama_profil_user = findViewById(R.id.nama_profil_user);
-        edit_profil_user = findViewById(R.id.edit_profil_user);
+        info_profil_user = findViewById(R.id.akun_profil_user);
+        nohp_user = findViewById(R.id.nohp_user);
 
-        pengaturan_profil_user = findViewById(R.id.pengaturan_profil_user);
         riwayat_profil_user = findViewById(R.id.riwayat_profil_user);
         bantuan_profil_user = findViewById(R.id.bantuan_profil_user);
         keluar_profil_user = findViewById(R.id.keluar_profil_user);
+        
+//        getUser("getHp");
+//        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+//        if (currentUser != null) {
+//            String uid = currentUser.getUid();
+//            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("User").child("Pencari").child(uid);
+//            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                    if (snapshot.exists()) {
+//                        String nama = snapshot.child("username").getValue(String.class);
+//                        String noHp = snapshot.child("phone").getValue(String.class);
+//
+//                        nama_profil_user.setText(nama);
+//                        nohp_user.setText(noHp);
+//                    }
+//                }
+//
+//                @Override
+//                public void onCancelled(@NonNull DatabaseError error) {
+//                    // Error handling
+//                }
+//            });
+//        }
 
-        edit_profil_user.setOnClickListener(new View.OnClickListener() {
+
+
+
+        info_profil_user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(profil_user.this, edit_profil.class);
@@ -51,13 +92,6 @@ public class profil_user extends AppCompatActivity {
             }
         });
 
-        pengaturan_profil_user.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(profil_user.this, "Belum di konfigurasi", Toast.LENGTH_SHORT).show();
-                return;
-            }
-        });
 
         riwayat_profil_user.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,5 +137,22 @@ public class profil_user extends AppCompatActivity {
             }
         });
     }
+
+//    private void getUser(String Hp) {
+//        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("User").child("Pencari");
+//        databaseReference.child(Hp).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+//            @Override
+//            public void onComplete(@androidx.annotation.NonNull Task<DataSnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    if (task.getResult().exists()) {
+//                        DataSnapshot dataSnapshot = task.getResult();
+//                         = String.valueOf(dataSnapshot.child("email").getValue());
+//                    } else {
+//                        Toast.makeText(Login.this, "Akun Pencari Tidak Ada", Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            }
+//        });
+//    }
 }
 
